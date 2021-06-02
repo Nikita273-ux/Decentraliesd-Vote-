6e52716d69e7ac7b1b9aea040015c21fa74ffb01
Nikita273-ux <83267924+Nikita273-ux@users.noreply.github.com>
2021-06-03 00:48:10 +0530

    Add files via upload

--- storage.sol
+++ storage.sol
@@ -2,15 +2,16 @@ pragma solidity 0.8.0;
 
 contract Storage {
     
+    enum VoterStatus {unverified, verified}
+    
     struct Voter{
-        
+        uint256 id;
        // address vAddress;
         string  fName;
         string  lName;
         uint8   age;
-       // uint8   vote;
+       
         bool    voted;
-      //  VoterStatus status;
     
         }
         
@@ -19,20 +20,47 @@ contract Storage {
         string Name;
         string lName;
         uint8  Age;
+        uint256 daddress;
         
     }
     
     uint256 public voterCounter;
     uint256 public delegateCounter;
     
-     mapping(address => Voter) public voters;
+     mapping(address => Voter) public voter;
       mapping(address=>delegate) public  delegates;
       
+    modifier checkVoter(){
+    require(voter[msg.sender].id != 0,"Voter doesn't exist");
+            _;
+    }
+    
+    modifier checkdelegate(){
+    require(delegates[msg.sender].daddress != 0,"Candidate doesn't exist");
+        _;
+    }
+
+    
+    modifier registered{
+      require(msg.sender != delegates[msg.sender].daddress,"try another address ");  
+        _;
+    }
+    
+    
       
-    function RegisterVoter(string memory _fname, string memory _lname, uint8 _age) public{
-        
-        
-        
+    function RegisterVoter(uint256 _id,string memory _fname, string memory _lname, uint8 _age) public{
+       
+        voterCounter++;
+         Voter memory vot;
+         vot.id      = voterCounter; 
+         vot.name    = _fname;
+         vot.name    = _lname;
+         vot.age     = _age;
+         vot.vadd    = msg.sender;
+         vot.isVoter = true;
+         voter[msg.sender] = vote;
+        voter.status = VoterStatus.unverified;
+
         
         
         
